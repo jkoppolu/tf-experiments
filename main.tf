8e254ad4b4b48985aed67eb315f3720a03e9238e
Johnbabu Koppolu <john@jodopay.com>
2023-10-02 17:41:43 +0530

    Updated VPC module version

--- main.tf
+++ main.tf
@@ -16,10 +16,10 @@ data "aws_ami" "app_ami" {
 
 
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2"
+  name    = "dev"
+  cidr    = "10.0.0.0/16"
 
   azs            = ["us-east-1a", "us-east-1b", "us-east-1c"]
   public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
