35735a48e2a2655f00cbbc22d6f4ed56f28f2af5
Johnbabu Koppolu <john@jodopay.com>
2023-10-02 17:06:48 +0530

    Use VPC Module

--- main.tf
+++ main.tf
@@ -14,8 +14,37 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs            = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name    = "blog"
+
+  vpc_id = module.vpc.public_subnets[0]
+
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+
 }
 
 resource "aws_instance" "blog" {
@@ -27,20 +56,5 @@ resource "aws_instance" "blog" {
   tags = {
     Name = "Learning Terraform"
   }
-}
-
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
-  name    = "blog"
-
-  vpc_id = data.aws_vpc.default.id
-
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules       = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
-
 }
 
